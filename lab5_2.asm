591b606caba180364546b3d176e98ae8c4448bd9
6174g <97239948+6174gh@users.noreply.github.com>
2022-02-10 20:44:45 +0530

    10022022

--- lab5_2.asm
+++ lab5_2.asm
@@ -1,36 +1,53 @@
 ; Permutation = n!/(n-r)!
 ; Procedure / modular concept - subroutine
+assume cs:code,ds:data
 data segment
-    org 2000h               ; points to that offset
-    n equ 6                 ; setting n as constant value 6
-    r equ 2
+	org 1000h
+	n equ 08h
+	r equ 02h
+	temp dw ?		
+	npr dw ?
+	ncr dw ?
 data ends
 code segment
-    assume cs:code ds:data
-    factorial proc near
-        mov cx,ax           ; value from function call moved to cx
-        rpt:dec cx      
-            mul cx          ; multiplied value available at ax
-            cmp cx,0001h    ; compared with 1 since decrement mode
-            jnz rpt
-    ret                     ; value returned
-    factorial endp          ; control trsnfered
-    start:                  ; main program
-        mov ax,n
-        call factorial      ; value at ax passed to function - n!
-        mov bx,ax           ; mov asnwer to bx
-        mov cx,r
-        sub ax,cx           ; n-r
-        mov ds:[2000], n    ; store data in memory
-        mov ds:[2001], r
-        mov ds:[2002], al
-        mov ds:[2003], bh
-        mov ds:[2004], bl
-        call factorial      ; (n-r)!
-        mov dx,ax
-        mov ds:[2005], dh
-        mov ds:[2006], dl
-        div dx
-        hlt
-end code
-    end start
+	start:
+	mov ax,data
+	mov ds,ax
+	xor ax,ax
+	xor dx,dx		
+	mov bx,n-r
+	call fact
+	mov temp,ax
+	xor ax,ax
+	lp2:mov bx,n
+	call fact
+	mov bx,temp
+	div bx			
+	mov npr,ax
+	mov bx,r
+	call fact
+	mov temp,ax
+	mov ax,npr
+	div temp
+	mov ncr,ax
+	mov bx,npr
+	hlt
+	fact proc
+		cmp bx,01h
+		jnz cnt
+		mov ax,01h
+		ret
+		cnt:
+		mov ax,01h
+		mov cx,00h
+		mov cx,bx
+		dec cx
+		mov ax, bx
+		proc_lp: dec bx
+			mul bx
+		loop proc_lp
+		ret
+	fact endp
+code ends
+end start
+
